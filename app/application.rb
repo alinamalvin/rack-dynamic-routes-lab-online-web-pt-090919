0cc5280bf2aeda44272bb533a15e89fb4d505791
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-05 15:50:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,25 @@
 class Application
- 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
- 
-      if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      item = Item.items.find{|i| i.name == item_name}
+    @@item = [Item.new("apples", 3.25), Item.new("banana", 2.25)]
 
-      if item
-        resp.write item.price
-      else 
+    def call(env)
+        resp =Rack::Response.new
+        req =Rack::Resonse.new(env)
+
+      if req.path.match(/items/)  
+        item_name = req.path.split("/items/").last
+        if item = @@item.find{|i| i.name == item_name}
+            resp.write item.price
+    #   else
+    #     resp.status = 400
+    #     resp.write "Item not found"
+
+
+         else
         resp.status = 400
-        resp.write "Item not found"
-      end
-      resp.finish
+        resp.write = "Route not found"
+        end
+        resp.finish
     end
-end 
 end
-
-
 
   
