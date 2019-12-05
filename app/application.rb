1aa4f9a672284af39b7676cf8ffc1d16a3a43d2b
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-05 15:45:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path=="/items/<ITEM NAME>"
-      resp.write "3.42"
+      resp.write "@@price"
     else
       resp.write "Route not found"
       resp.status = 404
