0de754b061aae107e0bf2b68f52552741eae3f07
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-05 15:42:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path=="/items/<ITEM NAME>"
       resp.write "3.42"
     else
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 404
     end
  
